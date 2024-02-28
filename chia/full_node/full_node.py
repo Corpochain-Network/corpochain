cc8f35dfc88cf01b8be3726c605e9bc3f24a7cec
serwisgalena <serwisgalena@gmail.com>
2024-02-28 14:14:10 +0100

    update full_node.py to latest bpx version

--- chia/full_node/full_node.py
+++ chia/full_node/full_node.py
@@ -408,6 +408,34 @@ class FullNode:
                 full_peak, state_change_summary, None
             )
             await self.peak_post_processing_2(full_peak, None, state_change_summary, ppp_result)
+            
+            tx_peak: BlockRecord = peak
+            while not tx_peak.is_transaction_block:
+                tx_peak = await self.blockchain.get_block_record_from_db(tx_peak.prev_hash)
+            
+            if tx_peak.height != 0:
+                try:
+                    full_tx_peak = await self.blockchain.get_full_block(tx_peak.header_hash)
+                    assert full_tx_peak is not None
+                    assert full_tx_peak.execution_payload is not None
+                    
+                    status = await self.execution_client.new_payload(full_tx_peak.execution_payload)
+                    if status == "INVALID" or status == "INVALID_BLOCK_HASH":
+                        raise RuntimeError(f"Payload status: {status}. Database is corrupted.")
+                    elif status != "VALID" and status != "SYNCING" and status != "ACCEPTED":
+                        raise RuntimeError("Unexpected payload status.")
+                    
+                    status = await self.execution_client.forkchoice_update(tx_peak)
+                    if status == "INVALID" or status == "INVALID_BLOCK_HASH":
+                        raise RuntimeError(f"Fork choice status: {status}. Database is corrupted.")
+                    elif status == "VALID":
+                        self.log.info("Execution chain head has been updated.")
+                    elif status == "SYNCING" or status == "ACCEPTED":
+                        self.log.info("Execution chain synchronization has been started.")
+                    else:
+                        raise RuntimeError("Unexpected fork choice status.")
+                except Exception as e:
+                    self.log.error(f"Exception in peak initialization: {e}")
         if self.config["send_uncompact_interval"] != 0:
             sanitize_weight_proof_only = False
             if "sanitize_weight_proof_only" in self.config:

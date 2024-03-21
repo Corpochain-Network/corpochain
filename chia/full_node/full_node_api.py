b0360d4409b04966edb33aea83f97748be86a8e8
serwisgalena <serwisgalena@gmail.com>
2024-03-21 15:21:56 +0100

    Fix beacon full_node

--- chia/full_node/full_node_api.py
+++ chia/full_node/full_node_api.py
@@ -479,7 +479,7 @@ class FullNodeAPI:
         self, new_sp: full_node_protocol.NewSignagePointOrEndOfSubSlot, peer: WSChiaConnection
     ) -> Optional[Message]:
         # Ignore if syncing
-        if self.beacon.sync_store.get_sync_mode() or self.beacon.execution_client.syncing:
+        if self.full_node.sync_store.get_sync_mode() or self.full_node.execution_client.syncing:
             return None
         if (
             self.full_node.full_node_store.get_signage_point_by_index(
